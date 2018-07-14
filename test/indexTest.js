f392ce64b901c54f608362e23439e5820fdd851b
alyssamf <alyssafeagans@gmail.com>
2018-07-14 21:02:21 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -22,15 +22,15 @@ describe('index.js', function () {
     });
   });
 
-  // describe('randomNumber', function () {
-  //   it('contains a random number greater than or equal to 0 and less than 20', function () {
-  //     expect(randomNumber).to.be.at.least(0).but.below(20);
-  //   });
+  describe('randomNumber', function () {
+    it('contains a random number greater than or equal to 0 and less than 20', function () {
+      expect(randomNumber).to.be.at.least(0).but.below(20);
+    });
 
-  //   it("invokes 'Math.random()'", function () {
-  //     expect(spyOnMathRandom.calledOnce).to.be.true;
-  //   });
-  // });
+    it("invokes 'Math.random()'", function () {
+      expect(spyOnMathRandom.calledOnce).to.be.true;
+    });
+  });
 
   describe('randomInteger', function () {
     it("contains the value of 'randomNumber' rounded down to the nearest integer", function () {
@@ -40,11 +40,11 @@ describe('index.js', function () {
     });
   });
 
-  // describe('randomUserID', function () {
-  //   it('contains a random integer between 1000000001 and 1000000020', function () {
-  //     expect(Number.isInteger(randomUserID)).to.be.true;
+  describe('randomUserID', function () {
+    it('contains a random integer between 1000000001 and 1000000020', function () {
+      expect(Number.isInteger(randomUserID)).to.be.true;
 
-  //     // expect(randomUserID).to.be.at.least(1000000001).and.at.most(1000000020);
-  //   });
-  // });
+      expect(randomUserID).to.be.at.least(1000000001).and.at.most(1000000020);
+    });
+  });
 });
